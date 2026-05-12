7870d2530f2f72f5588d216370b20e42d625b481
imityw <ebroterletskii@gmail.com>
2026-05-12 17:30:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Dispatcher, Bot, Router
 import asyncio
 from os import getenv
 from dotenv import load_dotenv
-from handlers.routes import router as main_router
-from students.database import init_db
+
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Я жив!"
 
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
 
 dp = Dispatcher()
-dp.include_router(main_router)
+router = Router()
+dp.include_router(router)
 
 async def main():
     await init_db()
-
     bot = Bot(token=TOKEN)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
+    threading.Thread(target=lambda: app.run(host='0.0.0.0', port=5000, debug=False)).start()
     asyncio.run(main())
